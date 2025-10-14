51f3e40d2e5b291ff38df7011737aafac317b3db
KOMMEHTATOP <basharovbmkz@gmail.com>
2025-10-14 16:58:08 +0500

    Увеличил цвета групп до 100 симфолов.

--- FlashcardsApp/Models/Group.cs
+++ FlashcardsApp/Models/Group.cs
@@ -7,9 +7,7 @@ public class Group
 {
     public Guid Id {get; set;}
     public required Guid UserId  {get; set;} 
-    [MaxLength(200)]
     public required string GroupName  {get; set;}
-    [MaxLength(100)]
     public required string GroupColor { get; set; } = "";
     public DateTime CreatedAt { get; set; }
     public int Order { get; set; } = 0;
