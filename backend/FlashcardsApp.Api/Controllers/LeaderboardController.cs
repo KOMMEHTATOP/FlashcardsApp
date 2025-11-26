32f216bc835f8021b0db1f8ddf014b2af1e3ae39
KOMMEHTATOP <basharovbmkz@gmail.com>
2025-11-26 17:55:47 +0500

    Test in dev2

--- backend/FlashcardsApp.Api/Controllers/LeaderboardController.cs
+++ backend/FlashcardsApp.Api/Controllers/LeaderboardController.cs
@@ -11,7 +11,7 @@ namespace FlashcardsApp.Api.Controllers
     [Authorize]
     public class LeaderboardController : BaseController  
     {
-        private readonly ILeaderboardBL _leaderboardBL;
+        private readonly ILeaderboardBL _leaderboardBL;  
 
         public LeaderboardController(UserManager<User> userManager, ILeaderboardBL leaderboardBL) : base(userManager)
         {
